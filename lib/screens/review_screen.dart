0d48c7ff6b746c886fd89ac084aefee8109b8010
dat060507 <hothanhdat869@gmail.com>
2026-07-09 16:55:44 +0700

    Create review_screen.dart

---
+++ lib/screens/review_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ReviewScreen extends StatelessWidget {
+  const ReviewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "Review Screen",
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+    );
+  }
+}
